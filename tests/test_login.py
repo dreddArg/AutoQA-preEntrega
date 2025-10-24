c9a49934dc565b5f9dbe71e98d291eee5ef3566f
Sergio Durelli <dredd_arg@hotmail.com>
2025-10-23 23:12:23 -0300

    modificar funcion incluye mensajes por consola

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,10 +8,12 @@ def test_login_to_inventory(login_with_driver):
         driver = login_with_driver   
 
         # Prueba de redireccionamiento a web inventario
-        assert "/inventory.html" in driver.current_url, "No se realizo la redireccion a web de inventario."
+        assert "/inventory.html" in driver.current_url, "No se realizo la redireccion a la web inventory.html"
+        print("Login y redireccionamiento exitoso.")
 
         # Prueba de Clase title = "Products"
-        assert driver.find_element(By.CLASS_NAME,"title").text == "Products"
+        assert driver.find_element(By.CLASS_NAME,"title").text == "Products", "No se encontró el subtitulo 'Products'"
+        print("Sub Titulo 'Products' verificado.")
 
     except Exception as e:
         print(f"Error test_login: {e}")
